b5840ad8a40aac84f884c1f166c9589e01fe9b00
phucciTr <miabam26@gmail.com>
2021-04-30 23:16:04 -0700

    Edit response string

--- server/routes/enrolled.js
+++ server/routes/enrolled.js
@@ -26,9 +26,9 @@ router.route('/enrolled/:id').put(async (req, res) => {
     let record = await Enrolled.findOne({ id: req.params.id });
     record.enrolled = req.body.enrolled;
     await record.save();
-    res.send(`updated tittle with id(${req.params.id}) to "${record.enrolled}"`);
+    res.send(`updated enrollment with id(${req.params.id}) to "${record.enrolled}"`);
 
-  } catch(e) { res.status(400).send(`failed to update tittle with id(${req.params.id})`); }
+  } catch(e) { res.status(400).send(`failed to update enrollment with id(${req.params.id})`); }
 });
 
 router.route('/enrolled/:id').delete((req, res) => {
